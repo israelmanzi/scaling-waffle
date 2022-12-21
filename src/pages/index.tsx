14fe3a544c1b16240a4a3b9b820af17d06802ab5
marcusglowe <marcus@create.xyz>
2022-12-20 23:49:09 -0500

    feat: init

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import styles from './index.module.scss';
+import { useCheckout } from '../mock-backend';
+
+const Index = () => {
+  const { items, buy } = useCheckout();
+
+  return (
+    <main className={styles.main}>
+      <h1>Create, Inc. Store</h1>
+      {/** @TODO: Not implemented */}
+    </main>
+  );
+};
+
+export default Index;
